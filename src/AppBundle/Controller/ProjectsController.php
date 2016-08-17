ebc36bd199afe125ff761c976025ae65b029446e
Richard <rik702@btinternet.com>
2016-08-17 12:23:41 +0100

    Simplified CSS to be Bootstrap only plus InvenioBase

--- src/AppBundle/Controller/ProjectsController.php
+++ src/AppBundle/Controller/ProjectsController.php
@@ -2,13 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\IvoProject;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ProjectsController extends Controller
 {
     /**
-     *@Route("/projects" )
+     *@Route("/projects",name="projects")
      */
     public function listAction()
     {
@@ -40,6 +41,41 @@ class ProjectsController extends Controller
         ));
     	// return $this->render('projects.html.twig', array("addressName"=>$waterCo->getIvoWatercoName()));
       // return $this->render('InvenioBundle:Default:index.html.twig',Array("addressName"=>$address->getIvoName()));
+    }
+
+    /**
+     *@Route("/projects/{projID}",name="projects_project")
+     */
+    public function detailAction($projID)
+    {
+        $waterCo = $this->getDoctrine()
+            ->getRepository('AppBundle:IvoWaterco')
+            ->findOneBy(['ivoWatercoId' => '2']);
+        // ->findAll();
+        //
+        $WaterCoName = $waterCo->getIvoWatercoName();
+        //
+        //dump($WaterCoName);
+        dump($waterCo);
+
+        $Projects = $this->getDoctrine()
+            ->getRepository('AppBundle:IvoProject')
+            ->findBy(array('ivoProjectId' => $projID));
+
+        dump($Projects);
+
+        $Loggers = $this->getDoctrine()
+            ->getRepository('AppBundle:IvoLoggerLocation')
+            ->findAll();
+        dump($Loggers);
+
+        return $this->render('projects\projects.html.twig',
+            array("WaterCoName" =>$WaterCoName,
+                "Projects" =>$Projects,
+                "Loggers" =>$Loggers
+            ));
+        // return $this->render('projects.html.twig', array("addressName"=>$waterCo->getIvoWatercoName()));
+        // return $this->render('InvenioBundle:Default:index.html.twig',Array("addressName"=>$address->getIvoName()));
     }
 
 }
